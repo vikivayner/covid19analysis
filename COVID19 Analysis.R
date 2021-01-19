bee39b85842ecae9c1e3a4fe53fa489e8b35ea8b
vikivayner <63611640+vikivayner@users.noreply.github.com>
2021-01-18 19:57:13 -0500

    Update COVID19 Analysis.R

--- COVID19 Analysis.R
+++ COVID19 Analysis.R
@@ -26,54 +26,62 @@ covid_df_all_states_daily_sum<-covid_df_all_states_daily %>%
 #head of sum of daily 
 covid_top_10 <- head(covid_df_all_states_daily_sum , 10)
 
+#renamed columns for top ten
+
 countries <- covid_top_10$Country_Region
 tested_cases <- covid_top_10$tested
 positive_cases <- covid_top_10$positive
 active_cases <- covid_top_10$active
 hospitalized_cases <- covid_top_10$hospitalized
+
+#renamed columns in countries df 
 names(tested_cases) <- countries 
 names(positive_cases) <-countries  
 names(active_cases) <- countries
 names(hospitalized_cases) <- countries
 
-
+#top 10 ratio 
 
 ratio <- positive_cases/tested_cases
 
+#top 10 ratio sorted by decreasing 
 ratio1<-sort(ratio, decreasing = TRUE)
 
-
+#top 3 countries who have the most positive tested cases
 positive_tested_top_3 <- head(ratio1, 3)
-
+#reading data
 positive_tested_top_3
 
-
+#rewriting the top 3 countries here. 
 united_kingdom <-c(0.11, 1473672, 166909, 0, 0) 
 
 united_states <- c(0.10, 17282363, 1877179, 0, 0)
 
 turkey <- c(0.08, 2031192, 163941, 2980960, 0)
 
-
+#creating a matrix 
 covid_mat <- rbind(united_kingdom, united_states, turkey)
 
-
+#naming the columns in matrix 
 colnames(covid_mat) <- c("Ratio", "tested", "positive", "active", "hospitalized")
-
+#printing matrix 
 covid_mat
 
+
+#answering dataquest questions
 question <- "Which countries have had the highest number of positive cases against the number of tests?"
 answer <- c("Positive tested cases" = positive_tested_top_3)
 
-
+#collecting all dataframes, vectors and matrices and putting them in a list. 
 dataframes <- list(covid_df, covid_df_all_states, covid_df_all_states_daily, covid_df_all_states_daily_sum, covid_top_10)
 matrices <- list(covid_mat)
 vectors <- list(active_cases, countries, hospitalized_cases, positive_cases, positive_tested_top_3,united_kingdom, united_states, turkey, tested_cases, vector_cols, ratio)
-
+#naming list 
 data_structure_list <- list( dataframes = dataframes, matrices = matrices, vectors = vectors)
 
+#creating final list
 
 covid_analysis_list <- list(question, answer, data_structure_list)
-
+#answering final question for Dataquest
 covid_analysis_list[[2]]
 
